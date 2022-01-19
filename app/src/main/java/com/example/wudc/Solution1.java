5c326d6833c15ceef7a7774177a7607fe9aae0e3
wlsskdbwls <wlsskdbwls@naver.com>
2022-01-20 03:21:08 +0900

    유진 위젯

--- app/src/main/java/com/example/wudc/Solution1.java
+++ app/src/main/java/com/example/wudc/Solution1.java
@@ -24,6 +24,32 @@ public class Solution1 extends AppCompatActivity {
         scrollView = (ScrollView) findViewById(R.id.scrollView);
         imageView = (ImageView) findViewById(R.id.imageView);
         scrollView.setHorizontalScrollBarEnabled(true);
+        ImageView nav_mediation, nav_record, nav_closer;
+
+        nav_mediation = (ImageView) findViewById(R.id.nav_mediation);
+        nav_record = (ImageView) findViewById(R.id.nav_record);
+        nav_closer = (ImageView) findViewById(R.id.nav_closer) ;
+
+        nav_mediation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MediationActivity.class);
+                startActivity(intent);
+            }
+        });
+        nav_record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                    //기록하기 액티비티와 연결해주세요
+                Intent intent = new Intent(getApplicationContext(), RecListViewActivity.class);
+                startActivity(intent);
+            }
+        });
+        nav_closer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
 
         Resources res = getResources();
         bitmap = (BitmapDrawable) res.getDrawable(R.drawable.solution1);
