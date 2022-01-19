011ffa7cf9370ecffd907f8d35e39081660d8158
yejuu <yyejuu00@gmail.com>
2022-01-19 22:59:40 +0900

    5차
    솔루션 메인 화면 기능 - 주기/솔루션 버튼 수정중

--- app/src/main/java/com/example/wudc/FragWeekDialogFragment.java
+++ app/src/main/java/com/example/wudc/FragWeekDialogFragment.java
@@ -1,11 +1,13 @@
 package com.example.wudc;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
@@ -19,19 +21,58 @@ public class FragWeekDialogFragment extends DialogFragment implements View.OnCli
         return fragweekdial;
     }
 
+    TextView choice[] = new TextView[3];
+    Integer choId[] = { R.id.btn_3day, R.id.btn_7day, R.id.btn_14day };
+    Integer max = 0;
+    Integer selected = 0;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.week_dialog, container);
-        ImageButton mConfrimBtn = (ImageButton) view.findViewById(R.id.btnSample);
+        TextView mConfrimBtn = (TextView) view.findViewById(R.id.btnSample);
 //        Button mConfrimBtn = (Button) view.findViewById(R.id.btnSample);
         mConfrimBtn.setOnClickListener(this);
         setCancelable(false);
+
+
+        for(int i = 0; i < choId.length; i++){
+            final int index;
+            index = i;
+            choice[index] = (TextView) view.findViewById(choId[index]);
+
+            choice[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(!choice[index].isSelected()){    //항목이 선택되어있지않으면
+                        if(max < 1){    //1개보다 적게 선택되어있으면
+                            max++;
+                            selected = index;
+                        } else if (max == 1) {
+                            choice[selected].setSelected(!choice[selected].isSelected());
+                            choice[selected].setTextColor(Color.parseColor("#191D30"));
+                            selected = index;
+                        }
+                        choice[index].setSelected(!choice[index].isSelected());
+                        choice[index].setTextColor(Color.parseColor("#FFFFFF"));
+
+                    } else {    //항목이 이미 선택되어있으면
+                        max--;
+                        choice[index].setSelected(!choice[index].isSelected());
+                        choice[index].setTextColor(Color.parseColor("#191D30"));
+                    }
+                }
+            });
+        }
+
+
         return view;
     }
 
     @Override
     public void onClick(View view) {
+
         dismiss();
 
     }
