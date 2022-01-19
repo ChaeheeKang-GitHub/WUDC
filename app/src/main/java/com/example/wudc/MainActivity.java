9e5e7fe028ef5e83d9154dd26f9d9cdef43fc757
yejuu <yyejuu00@gmail.com>
2022-01-19 16:06:25 +0900

    1차

--- app/src/main/java/com/example/wudc/MainActivity.java
+++ app/src/main/java/com/example/wudc/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.wudc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 버튼 만들어서 메인 넘어가도록 하기
+        Button imageButton = (Button) findViewById(R.id.btn1);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RecListViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
